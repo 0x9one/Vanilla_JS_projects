ea769487a01dc21ada83c44f8506f259f5421548
Swap <swap@swap.com>
2021-07-13 11:00:14 +0100

    Complete project 04 navbar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,18 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle')
+const links     = document.querySelector('.links')
+
+navToggle.addEventListener('click', function() {
+    // a normal way using add and remove 
+    if (links.classList.contains('show-links')) {
+        // links.classList.remove('show-links')
+    } else {
+        // links.classList.add('show-links')
+    }
+
+    // the second way and easy bu using toggle class
+    links.classList.toggle('show-links')
+})
